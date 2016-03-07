f9e4e0626069877cdd8459a98ec1942f2ea47bb8
Kat Timm <Kat Timm>
2016-03-07 23:34:51 +0100

    initial JS code, inc. an object, an array, and a for loop

---
+++ files/hello-world.js
@@ -0,0 +1,15 @@
+var kat = {
+	gender: "female",
+	country: "Deutschland"
+};
+
+var myArray = [10, 20, 40, 80, 160, 320];
+
+for (i = 0; i < myArray.length; i++) {
+	if (myArray[i] % 40 === 0) {
+		console.log("Hello, World!");
+	}
+	else {
+	    console.log("Hello, Moon!")
+	};
+};
