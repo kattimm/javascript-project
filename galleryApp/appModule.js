6ffb339c5c7a17acff786bc5929f312c309f9bbe
Kat Timm <Kat Timm>
2016-04-01 21:25:09 +0200

    added css styling, restructured layout (top nav in header)

--- galleryApp/appModule.js
+++ galleryApp/appModule.js
@@ -3,6 +3,7 @@ var app = angular.module("galleryApp", []);
 app.controller("galleryCtrl", function($scope) {
 	$scope.firstName = "Kat";
 	$scope.lastName = "Timm";
+	$scope.images = ["assets/images/a.jpg", "assets/images/b.jpg", "assets/images/c.jpg", "assets/images/d.jpg"];
 });
 
 
@@ -17,5 +18,19 @@ app.directive("sidebarDirective", function() {
 	return {
 		restrict: "E",
 		templateUrl: "app/sidebar.html"
+	};
+});
+
+app.directive("headerDirective", function() {
+	return {
+		restrict: "E",
+		templateUrl: "app/header.html"
+	};
+});
+
+app.directive("topnavDirective", function() {
+	return {
+		restrict: "E",
+		templateUrl: "app/topnav.html"
 	};
 });
